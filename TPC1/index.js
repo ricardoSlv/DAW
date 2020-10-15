174b148efb1fbd1e00d1e628c7fbe740f73555c0
ricardoslv <ricardoslv80@gmail.com>
2020-10-15 11:06:49 +0100

    Finish tpc1

--- TPC1/index.js
+++ TPC1/index.js
@@ -1,24 +1,26 @@
-const catApiKey='0c928b12-ce74-45d4-a024-85437887d2fe'
+const catApiKey = '0c928b12-ce74-45d4-a024-85437887d2fe'
 
 const url3 = 'https://api.thecatapi.com/v1/images/search?limit=6&breed_id=beng'
+
+let breedData=[]
 
 function showModal(element) {
     document.getElementById("img01").src = element.src;
     document.getElementById("modal01").style.display = "block";
 }
 
-function myFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
+function searchFilter() {
+    let input, filter,  b, i;
+    input = document.getElementById("search-input");
     filter = input.value.toUpperCase();
-    let div = document.getElementById("myDIV");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-      let txtValue = a[i].textContent || a[i].innerText;
+    let div = document.getElementById("search-options-container");
+    b = div.getElementsByTagName("button");
+    for (i = 0; i < b.length; i++) {
+      let txtValue = b[i].textContent || b[i].innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        a[i].style.display = "";
+        b[i].style.display = ""
       } else {
-        a[i].style.display = "none";
+        b[i].style.display = "none"
       }
     }
 }
@@ -30,9 +32,24 @@ async function fetchBreeds(){
     const data = await fetch(`https://api.thecatapi.com/v1/breeds`)
     const response = await data.json()
 
-    const breeds = response.map(x=>({id:x.id,name:x.name}))
-    console.log(breeds)
-    return breeds
+    breedData = response.map(x=>({id:x.id,name:x.name}))
+    console.log(breedData)
+
+    let div = document.getElementById("search-options-container");
+    const filterinput = div.firstElementChild 
+    div.innerHTML=""
+    div.appendChild(filterinput)
+    breedData.forEach(b=>{
+        const button = document.createElement('button')
+        button.classList.add('w3-bar-item','w3-button')
+        button.innerHTML = b.name
+        button.onclick = ()=>{
+            updateCats(b)
+            document.getElementById("search-input").value="";
+            searchFilter()
+        }
+        div.appendChild(button)
+    })
 }
 
 async function fetchCats(breed){
@@ -47,35 +64,32 @@ async function fetchCats(breed){
     return response
 }
 
+
+
 async function updateCats(breed){
-    const data = await fetchCats(breed)
+    const data = await fetchCats(breed.id)
     const catContainer=document.querySelector('#cat-card-container')
+    const breedHeader= catContainer.firstElementChild
+    breedHeader.innerHTML=breed.name
     catContainer.innerHTML = ''
+    catContainer.appendChild(breedHeader)
 
     data.forEach(e => {
         const catImg = document.createElement('img')
         catImg.classList.add('cat-img','w3-col','s12','m6','l4','w3-round','w3-center')
         catImg.onclick=()=>showModal(catImg)
         catImg.src=e.url
-        
-        //catImg.classList.add('cat-img')
-        // const card = document.createElement('div')
-        // card.classList.add('w3-card-4')
-        // const description = document.createElement('div')
-        // description.classList.add('w3-container','w3-center')
-        // const cardText = document.createElement('p')
-        // cardText.innerHTML="cat cat"
-        // description.innerHTML=cardText
-        // card.innerHTML=""
-        // card.appendChild(catImg)
-        // card.appendChild(description)
 
-        //catContainer.appendChild(card)
         catContainer.appendChild(catImg)
     });
 
 }
 
+async function updateCatsRandom(){
+    const randomBreedId = breedData[Math.floor(Math.random()*breedData.length)]
+    updateCats(randomBreedId)
+}
+
 fetchBreeds()
-updateCats('beng')
+updateCats({id:'beng',name:'Bengal'})
 
